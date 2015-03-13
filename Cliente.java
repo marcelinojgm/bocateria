5e7da5dcd215ccc3364d9d91bb95fcc07c303741
MJGM <MJGM@example.com>
2015-03-13 11:54:22 +0100

    codificada cabeceras demetodos de la clase cliente

--- Cliente.java
+++ Cliente.java
@@ -10,18 +10,56 @@ public class Cliente
     // instance variables - replace the example below with your own
     private int numeroCliente;
     private Cliente siguienteEnCola;
+    private int numeroDeBocadillos;
     private static int numeroClienteActual = 1;
 
     /**
      * Constructor for objects of class Cliente
+     * @param numeroDeBocadillos numerode bocadillos que va acomprar
      */
-    public Cliente()
+    public Cliente(int numeroDeBocadillos)
     {
-       numeroCliente = 0;
-       siguienteEnCola = null;
-       
-        
+        numeroCliente = 0;
+        siguienteEnCola = null;
+        this.numeroDeBocadillos = numeroDeBocadillos;
+
     }
 
-   
+    /**
+     * 
+     */
+    public Cliente getSiguienteEnLaCola(){
+        return null;
+    }
+
+    /**
+     * retorna el numero de bocadillos que compra
+     * @return numeroDeBocadillos
+     */
+    public int getNumeroDeBocadillos(){
+        return 0;
+    }
+
+    /**
+     * numero del cliente
+     * @return retorna el numero del cliente
+     */
+    public int intgetNumeroCliente(){
+        return 0;
+    }
+
+    /**
+     * datos del cleinte
+     */
+    public String toString(){
+        return "";
+
+    }
+
+    /**
+     * selecciona el siguiente cliente enla cola
+     */
+    public void setSiguienteEnLaCola(Cliente cliente){
+
+    }
 }
